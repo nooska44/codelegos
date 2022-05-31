d77d8bcd418589d168d16165886fd39f60d38e93
Anouska Gievano <nooska44@gmail.com>
2022-05-31 11:09:24 +0800

    Added files and counter code snippet

---
+++ javascriptLegos/js/main.js
@@ -0,0 +1,13 @@
+//Create a counter
+
+//Example
+let nums = [42,19,77,33,57]
+
+let sum = nums.reduce( (acc,c) => acc + c, 0  )
+
+alert(sum)
+
+//code snippet
+
+nums.reduce( (acc,c) => acc + c, 0  )
+
