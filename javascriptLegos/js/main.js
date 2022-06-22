d60e85ec14f5a5daf49fc26787aec75418e62140
Anouska Gievano <77055815+nooska44@users.noreply.github.com>
2022-06-22 17:50:16 +0800

    call a function in a variable declaration

--- javascriptLegos/js/main.js
+++ javascriptLegos/js/main.js
@@ -1,35 +1,3 @@
-//Create a counter
-
-//Example
-let nums = [42,19,77,33,57]
-
-let sum = nums.reduce( (acc,c) => acc + c, 0  )
-
-alert(sum)
-
-//code snippet
-
-nums.reduce( (acc,c) => acc + c, 0  )
-
-// Other ways to count
-
-//example 
-function countDevelopers(list) {
-    let europeanJSDev = 0;
-    for(let i = 0; i < list.length; i++){
-      
-    if (list[i].continent === 'Europe' && list[i].language === 'JavaScript'){
-        europeanJSDev++
-        }
-    }
-    return europeanJSDev
-}
-
-//code snippet
-
-europeanJSDev++
-
-
 //You can call a function in a variable declaration  
 
 function multThreeNum(num1, num2, num3){
