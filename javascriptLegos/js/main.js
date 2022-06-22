7cd9fac4073f4de637187da57a09f76eb7682cf8
Anouska Gievano <77055815+nooska44@users.noreply.github.com>
2022-06-22 14:34:17 +0800

    calling a function in a variable declaration

--- javascriptLegos/js/main.js
+++ javascriptLegos/js/main.js
@@ -28,3 +28,13 @@ function countDevelopers(list) {
 //code snippet
 
 europeanJSDev++
+
+
+//You can call a function in a variable declaration  
+
+function multThreeNum(num1, num2, num3){
+    return(num1 * num2 * num3)
+}
+
+let returnedVal = multThreeNum(2,3,2)
+console.log(returnedVal)
