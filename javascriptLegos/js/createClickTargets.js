f85e160e86ea904fd0f0d733334a7b09548d54fd
Anouska Gievano <77055815+nooska44@users.noreply.github.com>
2022-06-25 20:01:27 +0800

    (click.target.classList.contains('rose'))

---
+++ javascriptLegos/js/createClickTargets.js
@@ -0,0 +1,4 @@
+function checkForRose(click){
+	if(click.target.classList.contains('rose')){ 
+    // set click target as multiple smurfs listening 
+    // why dont need a dot for class - already established looking for a class with classlist
