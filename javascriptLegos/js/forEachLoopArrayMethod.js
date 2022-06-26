d1bedc41719719af78bf9551d673581a21ae5a24
Anouska Gievano <77055815+nooska44@users.noreply.github.com>
2022-06-26 17:38:06 +0800

    bestColours.forEach((x, i) => console.log(x, i))

---
+++ javascriptLegos/js/forEachLoopArrayMethod.js
@@ -0,0 +1,3 @@
+let bestColours = ['green', 'red', 'blue']
+
+bestColours.forEach((x, i) => console.log(x)) // loops through and console.logs x if you have console.log(x, i) it console.logs x and index.
