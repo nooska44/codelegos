f5eed3da349eafca6e64640ba41578ed239201c9
Anouska Gievano <77055815+nooska44@users.noreply.github.com>
2022-06-26 17:52:53 +0800

    document.querySelector("#claire").style.display = 'none'

---
+++ javascriptLegos/js/hideImages.js
@@ -0,0 +1,36 @@
+document.querySelector('#finalRose').addEventListener('click', hide)  //add a smurf to run hide
+
+function hide(){
+	document.querySelector("#claire").style.display = 'none'   //style.display to none removes it. Used id to identify the image to remove
+	document.querySelector("#sharleen").style.display = 'none'
+}
+
+
+// or
+
+const andi = document.querySelector('#andi')
+const claire = document.querySelector('#claire')
+const sharleen = document.querySelector('#sharleen')
+
+document.querySelector('#andiNext').addEventListener('click', andiNext)
+document.querySelector('#claireNext').addEventListener('click', claireNext)
+document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
+
+function andiNext(){
+	claire.classList.add('hidden')
+	sharleen.classList.add('hidden')
+	andi.classList.toggle('hidden')
+}
+
+function claireNext(){
+	andi.classList.add('hidden')
+	sharleen.classList.add('hidden')
+	claire.classList.toggle('hidden')
+}
+
+function sharleenNext(){
+	claire.classList.add('hidden')
+	andi.classList.add('hidden')
+	sharleen.classList.toggle('hidden')
+}
+
