f59e233deee2b246dd90a1edeaed49d4048efd03
Anouska Gievano <77055815+nooska44@users.noreply.github.com>
2022-06-26 16:39:43 +0800

    return arr.sort((a, b) => a.charCodeAt([a.length-1]) - b.charCodeAt([b.length-1]))

---
+++ javascriptLegos/js/sortArrayWithSpecificCharacter.js
@@ -0,0 +1,1 @@
+return arr.sort((a, b) => a.charCodeAt([a.length-1]) - b.charCodeAt([b.length-1])) //sorts arr and assign a and b. then look for character at specific index - found the last one by using length - 1
