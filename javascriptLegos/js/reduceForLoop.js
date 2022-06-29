02cd31f911b40861b5284239a68ffb6be6719c1f
Anouska Gievano <77055815+nooska44@users.noreply.github.com>
2022-06-29 19:43:19 +0800

    return nums.reduce(function(sum, n){return (n*n) + sum}, 0)

--- javascriptLegos/js/reduceForLoop.js
+++ javascriptLegos/js/reduceForLoop.js
@@ -1,5 +1,5 @@
 function squareSum(nums){                          // function to square numbers and add all of the squared numbers together
   return nums.reduce(function(sum, n){             // use the reduced function on nums. sum is the first/previous value, n is the current value.
-    return (n*n) + sum;                            // the current value is squared and then added to the previous value
+    return (n*n) + sum                             // the current value is squared and then added to the previous value
   }, 0)                                            // 0 is the starting value
 }
